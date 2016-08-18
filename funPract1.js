ee4f42c948fcdcaab92b275ce7ddbc4ce8792b61
Student <Student@Mac-mini-7.local>
2016-08-18 11:45:22 -0600

    midday push

--- funPract1.js
+++
@@ -1,9 +0,0 @@
-var count = 10;
- 
-for (var i = 0; i < count; i++) {
-    saySomething();
-}
- 
-function saySomething() {
-    document.writeln("hello!");
-}
---
+++ script.js
@@ -0,0 +1,14 @@
+var count = 3;
+ 
+for (var i = 0; i < count; i++) {
+    saySomething();
+}
+ 
+function saySomething() {
+    document.writeln("Five");
+}
+
+// var word = "hello";
+// function last-letter() {
+//     document.writein(word.charAt(4);
+// }
