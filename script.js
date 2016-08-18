677bb10715b532daed83fac494a71b9d630a9b84
Student <Student@Mac-mini-7.local>
2016-08-18 14:04:13 -0600

    update

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ function saySomething() {
 }
 
 // var word = "hello";
-// function last-letter() {
-//     document.writein(word.charAt(4);
+//  var last = word.charAt(word.length - 1);
+// function lastLetter() {
+   
+//     // alert('The last character is:' + last);
+//     document.writeIn("The last letter is " + last);
 // }
+
+var square = function(x) {
+  return x * x;
+};
+document.writeln(square(3));
+document.writeln(square(5));
